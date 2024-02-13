97fd40b696c57155913ef1568ababbecadcffb5e
Amiltonxavier <amiltonxavier1999@gmail.com>
2024-02-13 10:50:11 +0100

    firt commit

---
+++ README.md
@@ -0,0 +1,75 @@
+# Notas de Voz Web App
+
+## Descrição
+
+O Notas de Voz Web App é uma aplicação web inovadora que permite aos usuários fazer anotações de forma prática utilizando o recurso de reconhecimento de voz. Com a capacidade de gravar e transcrever automaticamente o que é falado no microfone, esta aplicação proporciona uma experiência eficiente e moderna para capturar pensamentos e ideias rapidamente.
+
+## Demonstração
+
+[![Demonstração](/public/home.gif)](/public//openDialog.gif)
+
+Confira uma demonstração ao vivo [aqui](link-da-aplicacao).
+
+## Funcionalidades
+
+- **Gravação por Voz:** Utilize o microfone para gravar suas ideias de forma rápida e fácil.
+- **Transcrição Automática:** As falas são transcritas em tempo real, proporcionando uma experiência de anotação eficiente.
+- **Interface Intuitiva:** Design limpo e intuitivo para facilitar a navegação e utilização.
+
+## Pré-requisitos
+
+Certifique-se de ter o Node.js instalado em seu sistema.
+
+## Instalação
+
+1. Clone o repositório:
+
+    ```bash
+    git clone https://github.com/Amiltonxavier/note-speechRecognition.git
+    ```
+
+2. Navegue até o diretório do projeto:
+
+    ```bash
+    cd nome-do-repositorio
+    ```
+
+3. Instale as dependências:
+
+    ```bash
+    npm install
+    ```
+
+## Uso
+
+1. Inicie a aplicação executando o seguinte comando:
+
+    ```bash
+    npm run dev
+    ```
+
+2. Abra o navegador e acesse [http://localhost:5173](http://localhost:5173).
+
+3. Pressione o botão de Adicionar nota. 
+
+4. Escolhe a forma que deseja gravar suas notas.
+
+  a. Se caso for Escrita começa a escrever.
+  b. Caso for áudio, fale e veja suas palavras sendo transcritas para a área de notas.
+
+5. Para parar a gravação, pressione o botão de gravação p/ de gravar.
+
+## Tecnologias Utilizadas
+
+- **React (Vite):** Desenvolvimento eficiente e rápido.
+- **Tailwind CSS:** Estilização moderna e responsiva.
+- **SpeechRecognition API:** Reconhecimento de voz para captura de áudio.
+- **Sonner:** Toast de sucess ou error.
+
+## Contribuição
+
+Contribuições são bem-vindas! Sinta-se à vontade para abrir uma issue ou enviar um pull request.
+
+## Licença
+
+Este projeto é licenciado sob a [MIT License](LICENSE).
---
+++ src/App.tsx
@@ -0,0 +1,78 @@
+import { ChangeEvent, useState } from "react";
+import logo from "./assets/logo-nlw-expert.svg";
+import { NewNoteCard } from "./components/NewNoteCard";
+import { NotesCard } from "./components/Notes-card";
+
+
+interface Note{
+  id: string,
+  date: Date,
+  content: string
+}
+
+export function App() {
+  const [search, setSearch] = useState('')
+  const [notes, setNotes] = useState<Note[]>(() => {
+    const notesOnStorage = localStorage.getItem('notes')
+    if(notesOnStorage){
+      return JSON.parse(notesOnStorage)
+    }
+
+    return []
+  });
+
+  function onNoteCreated(content: string){
+    const newNote = {
+      id: crypto.randomUUID(),
+      date: new Date(),
+      content
+    }
+
+    const notesArray = [newNote, ...notes];
+    setNotes(notesArray); 
+    localStorage.setItem('notes', JSON.stringify(notesArray)) //LocalStorage não aceita array por isto deve se converter em json
+    
+  }
+
+  function onNoteDelete(id: string){
+      const notesArray = notes.filter(node => {
+        node.id !== id
+      })
+      setNotes(notesArray);
+
+      localStorage.setItem('notes', JSON.stringify(notesArray)) 
+  }
+
+  function handleSearch(e: ChangeEvent<HTMLInputElement>){
+    const query = e.target.value;
+    setSearch(query);
+  }
+
+  const filterNotes = search !== '' ? notes.filter(note =>note.content.toLocaleLowerCase().includes(search.toLocaleLowerCase())): notes
+
+  return (
+    <div className="mx-auto max-w-6xl my-12 space-y-6 px-5">
+      <img src={logo} alt="NLW EXPERT" />
+
+      <form action="" method="get" className="w-full ">
+        <input
+          type="text"
+          name=""
+          placeholder="Busque em suas notas..."
+          className="w-full bg-transparent text-3xl font-semibold tracking-tighter placeholder:text-slate-500 outline-none"
+          onChange={handleSearch}
+        />
+      </form>
+
+      <div className="h-px bg-slate-700" />
+
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 auto-rows-[250px]">
+        <NewNoteCard onNoteCreted={onNoteCreated} />
+
+        {filterNotes.map((note) => {
+          return <NotesCard key={note.id} note={note} onNoteDelete={onNoteDelete} />;
+        })}
+      </div>
+    </div>
+  );
+}
